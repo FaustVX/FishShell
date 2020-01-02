582289e0d88aa3dc4a7fe08cdbcadccb58419d08
FaustVX <jodu035@gmail.com>
2020-01-02 23:25:32 +0100

    Removed PlexToken

--- functions/updatePlex.fish
+++ functions/updatePlex.fish
@@ -1,7 +1,7 @@
-function updatePlex --argument plexToken
+function updatePlex
 	pushd /tmp
-	wget -O plex.deb "https://plex.tv/downloads/latest/5?channel=16&build=linux-armv7hf_neon&distro=debian&X-Plex-Token=$plexToken"
 	sudo systemctl stop plexmediaserver.service
+	wget -O plex.deb "https://plex.tv/downloads/latest/5?channel=16&build=linux-armv7hf_neon&distro=debian"
 	sudo apt install ./plex.deb
 	sudo systemctl restart plexmediaserver.service
 	sudo rm plex.deb
